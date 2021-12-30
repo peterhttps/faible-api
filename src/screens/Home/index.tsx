a0f2c3efe066713d863f5c3e9757126cf4a1f6ce
peterhttps <plrf100@gmail.com>
2021-12-29 23:28:24 -0300

    feature(styled_navigation): setting up react-navigation and styled-components

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import { Wrapper } from './styles';
+
+const Home: React.FC = () => {
+  return (
+    <Wrapper>
+      <Text>Home Page</Text>
+    </Wrapper>
+  );
+}
+
+export default Home;
