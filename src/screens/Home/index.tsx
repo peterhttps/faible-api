2feea6bf83e61dfe815dcc2dabd01d509f3e88fb
peterhttps <plrf100@gmail.com>
2022-01-07 23:21:00 -0300

    feature(refresh): adding refresh to main home view

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
+import { RefreshControl } from 'react-native';
 import ExploreComponent from '../../components/ExploreComponent';
 import FableList from '../../components/FableList';
 import RecentsCarousel from '../../components/RecentsCarousel';
 
 import { DescriptionTitle, TitleOrange, TitlePurple, Wrapper } from './styles';
 
+const wait = (timeout: number) => {
+  return new Promise(resolve => setTimeout(resolve, timeout));
+};
 const Home: React.FC = () => {
+  const [refreshing, setRefreshing] = React.useState(false);
+
+  const onRefresh = React.useCallback(() => {
+    setRefreshing(true);
+    wait(2000).then(() => setRefreshing(false));
+  }, []);
+
   return (
-    <Wrapper>
+    <Wrapper
+      refreshControl={
+        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+      }
+    >
       <TitlePurple>
         F<TitleOrange>ai</TitleOrange>ble
       </TitlePurple>
