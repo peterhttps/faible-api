cf3175733790005c6ba30b214fc4e0e2d4bf33c9
peterhttps <plrf100@gmail.com>
2022-01-07 22:24:33 -0300

    fix(comments): removing comments

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,16 +1,11 @@
 import React from 'react';
 import ExploreComponent from '../../components/ExploreComponent';
 import FableList from '../../components/FableList';
-import { useStories } from '../../components/hooks/useStories';
 import RecentsCarousel from '../../components/RecentsCarousel';
 
 import { DescriptionTitle, TitleOrange, TitlePurple, Wrapper } from './styles';
 
 const Home: React.FC = () => {
-  // const { stories } = useStories();
-
-  // stories.map(sto => console.log(`${sto.title}\n`));
-
   return (
     <Wrapper>
       <TitlePurple>
